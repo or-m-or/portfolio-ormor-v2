ffaf2beaad46130c7a043183639c45e844ef54e7
ormor <htth815@gmail.com>
2025-04-12 18:08:05 +0900

    🔧 CONFIG: 전역 폰트 및 배경색 설정 - 전역 폰트: Pretendard - 전역 색상: 다크모드 기본설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
     <div className='text-3xl'>
-      <p className='font-thin'>PROJECT SKILL CONTACT</p>
-      <p className='font-extralight'>Pretendard Test 프리텐다드</p>
-      <p className='font-light'>Pretendard Test 프리텐다드</p>
-      <p className='font-regular'>Pretendard Test 프리텐다드</p>
-      <p className='font-medium'>Pretendard Test 프리텐다드</p>
-      <p className='font-yclover font-semibold'>PROJECT SKILL CONTACT yclover</p>
+      <p className='font-thin'>전역 글꼴 Test</p>
+      <p className='font-extralight'>전역 글꼴 Test</p>
+      <p className='font-light'>전역 글꼴 Test</p>
+      <p className='font-regular'>전역 글꼴 Test</p>
+      <p className='font-medium'>전역 글꼴 Test</p>
+      <p className='font-yclover font-semibold'>PROJECT SKILL CONTACT 클로바</p>
       <p className='font-moneygraphy font-bold'>PROJECT SKILL CONTACT 머니그래피</p>
       <p className='font-suit font-extrabold'>PROJECT SKILL CONTACT 수트</p>
-      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
-      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
-      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-yclover font-black'>PROJECT SKILL CONTACT</p>
+      <p className='font-moneygraphy font-black'>PROJECT SKILL CONTACT</p>
+      <p className='font-suit font-black'>PROJECT SKILL CONTACT</p>
       <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
       <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
       <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
