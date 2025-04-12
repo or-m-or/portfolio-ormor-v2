90dfe5a36b8718e9d6c9debcb4e9715bdf2e966d
ormor <htth815@gmail.com>
2025-04-12 16:22:11 +0900

    ✨ FEAT: 네비게이션바 구현 - `/`경로로 이동하는 아이콘 추가 - Project, Skill, Contact 페이지로 이동하는 링크추가 - 화면 스크롤 시, glass 효과 적용 - 모바일사이즈 화면에서 햄버거 메뉴로 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,41 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-moneygraphy">
-    <p className="font-thin">Pretendard Test 프리텐다드</p>
-    <p className="font-extralight">Pretendard Test 프리텐다드</p>
-    <p className="font-light">Pretendard Test 프리텐다드</p>
-    <p className="font-regular">Pretendard Test 프리텐다드</p>
-    <p className="font-medium">Pretendard Test 프리텐다드</p>
-    <p className="font-yclover font-semibold">Pretendard Test 프리텐다드</p>
-    <p className="font-bold">Moneygraphy 머니그래피</p>
-    <p className="font-suit font-extrabold">SUIT Test 수트</p>
-    <p className="font-pretendard font-black">Pretendard Test 프리텐다드</p>
-  </div>
+    <div className='text-3xl'>
+      <p className='font-thin'>PROJECT SKILL CONTACT</p>
+      <p className='font-extralight'>Pretendard Test 프리텐다드</p>
+      <p className='font-light'>Pretendard Test 프리텐다드</p>
+      <p className='font-regular'>Pretendard Test 프리텐다드</p>
+      <p className='font-medium'>Pretendard Test 프리텐다드</p>
+      <p className='font-yclover font-semibold'>PROJECT SKILL CONTACT yclover</p>
+      <p className='font-moneygraphy font-bold'>PROJECT SKILL CONTACT 머니그래피</p>
+      <p className='font-suit font-extrabold'>PROJECT SKILL CONTACT 수트</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-suit font-extrabold'>PROJECT SKILL CONTACT 수트</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+      <p className='font-pretendard font-black'>PROJECT SKILL CONTACT 프리텐다드</p>
+    </div>
   );
 }
